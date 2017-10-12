76c8ea53e3996d478e9b0eb2005d2674311a4cb1
Anders Ragfelt <anders.ragfelt@live.se>
2017-10-12 23:41:35 +0200

    Refaktorering phase 2

--- src/main/java/dsv/inte2017g11/roguelib/GameMap.java
+++ src/main/java/dsv/inte2017g11/roguelib/GameMap.java
@@ -1,34 +1,47 @@
 package dsv.inte2017g11.roguelib;
 
+import java.util.Random;
+
 public class GameMap {
     //the size x*x of the man
-    private final int SIZE;
+    private final int WIDTH;
+    private final int HEIGHT;
     private Tile[][] field;
+    private Random rand = new Random();
 
-    public GameMap(int size) {
-        if (size > 0) {
-            SIZE = size;
-            fillMapField(size);
-        } else throw new IndexOutOfBoundsException();
+    public GameMap(int width,int height) {
+        if (width <= 0 || height <= 0)
+        	throw new IndexOutOfBoundsException();
+        this.WIDTH = width;
+        this.HEIGHT = height;
+        fillMapField(width,height);
     }
 
-    private void fillMapField(int s) {
-        field = new Tile[s][s];
-        for (int i = 0; i < s; i++) {
-            for (int j = 0; j < s; j++) {
-                field[i][j] = new Tile(i, j);
+    private void fillMapField(int x,int y) {
+        field = new Tile[x+1][y+1];
+        for (int i = 0; i <= x; i++) {
+            for (int j = 0; j <= y; j++) {	
+                field[i][j] = new Tile(randomTerrain());
             }
         }
     }
+    
+    private int randomTerrain(){
+    	return rand.nextInt(10);
+    }
 
     public Tile getPosition(int x, int y) {
-        if (x < 0 || y < 0 || x > SIZE || y > SIZE)
+        if (x < 0 || y < 0 || x > WIDTH || y > HEIGHT)
             throw new IndexOutOfBoundsException();
         else
             return field[x][y];
     }
 
-    public int getSize() {
-        return SIZE;
+    public int getWidth() {
+        return WIDTH;
+    }
+    
+    public int getHeight() {
+        return HEIGHT;
     }
 }
