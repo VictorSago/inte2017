9b026faf1e07855bff922db094f61cc541a5275b
Anders Ragfelt <anders.ragfelt@live.se>
2017-10-12 23:45:07 +0200

    Refaktorering phase2 m

--- src/test/java/dsv/inte2017g11/roguelib/TileTest.java
+++ src/test/java/dsv/inte2017g11/roguelib/TileTest.java
@@ -8,8 +8,33 @@ public class TileTest {
 
     @Test
     public void testTileCreate(){
-        Tile p = new Tile();
+        Tile p = new Tile(2);
         assertNotNull(p);
+    }
+    
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testTileInvalid(){
+        Tile p = new Tile(-1);
+    }
+    
+    @Test
+    public void testGetTerrain(){
+    	Tile p = new Tile(2);
+    	assertEquals(2,p.getTerrain());
+    }
+    
+    
+    @Test
+    public void testSetTerrain(){
+    	Tile p = new Tile(2);
+    	p.setTerrain(3);
+    	assertEquals(3,p.getTerrain());
+    }
+    
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetTerrainFail(){
+    	Tile p = new Tile(2);
+    	p.setTerrain(-2);
     }
 
     /*@Test
@@ -25,10 +50,7 @@ public class TileTest {
         assertEquals(10, p.getY());
     }*/
 
-    /*@Test(expected = IndexOutOfBoundsException.class)
-    public void testTileInvalidXCoordinate(){
-        Tile p = new Tile(-1,2);
-    }*/
+
 
     /*@Test(expected = IndexOutOfBoundsException.class)
     public void testTileInvalidYCoordinate(){
@@ -44,37 +66,37 @@ public class TileTest {
 
     @Test
     public void testTileHashCode(){
-        Tile p = new Tile();
-        Tile q = new Tile();
+        Tile p = new Tile(1);
+        Tile q = new Tile(1);
         assertEquals(true, p.hashCode()==q.hashCode());
     }
 
-    /*@Test
-    public void testTileHashCodeOnInvertedCoordinateValues(){
-        Tile p = new Tile(4,2);
-        Tile q = new Tile(2,4);
+    @Test
+    public void testTileHashCodeFail(){
+        Tile p = new Tile(4);
+        Tile q = new Tile(2);
         assertEquals(false, p.hashCode()==q.hashCode());
-    }*/
+    }
 
     @Test
     public void testTileEqualsMethod(){
-        Tile p = new Tile();
-        Tile q = new Tile();
+        Tile p = new Tile(3);
+        Tile q = new Tile(3);
         assertEquals(true, p.equals(q));
     }
 
-    /*@Test
+    @Test
     public void testTileFalseEqualsMethod(){
-        Tile p = new Tile (2,4);
-        Tile q = new Tile(4,2);
+        Tile p = new Tile(2);
+        Tile q = new Tile(4);
         assertEquals(false, p.equals(q));
-    }*/
+    }
 
     @Test
     public void testTileEqualsAnotherObject(){
-        Tile p = new Tile();
-        GameMap m = new GameMap(2);
-        assertEquals(false, p.equals(m));
+        Tile p = new Tile(4);
+        String s = "teststring";
+        assertEquals(false, p.equals(s));
     }
 
 }
