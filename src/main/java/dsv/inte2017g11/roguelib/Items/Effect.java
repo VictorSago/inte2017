02d24f88d12f502ab2b82adf82bae11369a54089
wrgl0 <marcus.wargloo@gmail.com>
2017-10-13 14:34:40 +0200

    w. documentation

--- src/main/java/dsv/inte2017g11/roguelib/Items/Effect.java
+++ src/main/java/dsv/inte2017g11/roguelib/Items/Effect.java
@@ -1,7 +1,17 @@
 package dsv.inte2017g11.roguelib.Items;
 
+/*
+enum class that's used for controlling what attribute of a game-character
+that is effected by the item
+Health - gives the character more health/armor
+Magic - affecs the characters magic
+Speed - affects the characters speed
+Damage - affects how the character deals damage (power points)
+
+ */
 public enum Effect {
     HEALTH,
     MAGIC,
-    SPEED
+    SPEED,
+    DAMAGE
 }
--- src/main/java/dsv/inte2017g11/roguelib/Items/GearItem.java
+++ src/main/java/dsv/inte2017g11/roguelib/Items/GearItem.java
@@ -1,11 +1,19 @@
 package dsv.inte2017g11.roguelib.Items;
 
+/*
+GearItem is a subclass of the item class
+it takes name, weigth, gearHP and effect as argument
+the gearHP is how much hp the gear has
+effect is going to be implemented to not only give addition to the
+  characters health when being equipped but also to affect another
+  ability(more magic, slows down, lowers strength etc)
+ */
+
 public class GearItem extends Item {
 
     private int weight;
     private int gearHP;
     private Effect effect;
-
 
     public GearItem(String name, int weight, int gearHP, Effect e) {
         super(name);
