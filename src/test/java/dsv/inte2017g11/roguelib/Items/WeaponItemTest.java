d187f815481c850547b769401d331ac210d2f1c3
wrgl0 <marcus.wargloo@gmail.com>
2017-10-14 12:09:10 +0200

    started on item-subclass weaponitem

---
+++ src/test/java/dsv/inte2017g11/roguelib/Items/WeaponItemTest.java
@@ -0,0 +1,12 @@
+package dsv.inte2017g11.roguelib.Items;
+
+import org.junit.Test;
+
+public class WeaponItemTest {
+
+    @Test
+    public void testCreateWeapon(){
+        WeaponItem w = new WeaponItem("Axe", 5);
+
+    }
+}
