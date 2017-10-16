358c396e98bd8dc5f21abc5af14851d087b1ab17
Tove <tdv-@live.se>
2017-10-16 13:04:35 +0200

    Pets pets pets

--- src/main/java/dsv/inte2017g11/roguelib/Pets/Pets.java
+++ src/main/java/dsv/inte2017g11/roguelib/Pets/Pets.java
@@ -4,11 +4,17 @@ public class Pets {
     private String name;
     private int currentHealth;
     private int currentSpeed;
+    private int maxHealth;
+    private int level;
+    private boolean available;
 
     public Pets(String name, int health, int speed) {
         this.name = name;
         this.currentHealth = health;
+        this.maxHealth = currentHealth;
         this.currentSpeed = speed;
+        this.level = 1;
+        this.available = true;
     }
 
     public int getCurrentHealth() {
@@ -17,5 +23,64 @@ public class Pets {
 
     public int getCurrentSpeed() {
         return currentSpeed;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void newName(String newName) {
+        this.name = newName;
+    }
+
+    public void takeDamage(int damage) {
+        currentHealth -=damage;
+        if (currentHealth<0){
+            currentHealth = 0;
+        }
+        if(currentHealth==0){
+            deadPet();
+        }
+    }
+
+    private void deadPet() {
+        available=false;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void heal(int healItem) {
+        if(isAvailable()) {
+            currentHealth += healItem;
+            if (currentHealth > maxHealth) {
+                currentHealth = maxHealth;
+            }
+        }
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    //TODO kan speed öka? fylla upp currentHealth vid levelup?
+    public void levelUp() {
+        level+=1;
+        maxHealth+=10;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void revive() {
+        available=true;
+        currentHealth=10;
+    }
+
+    public void phoenixDown() {
+        available=true;
+        currentHealth=maxHealth;
     }
 }
